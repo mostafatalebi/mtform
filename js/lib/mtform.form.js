5586aee00e464fbd36bf68d9aa948cd63a4c4869
mostafatalebi <most.talebi@gmail.com>
2014-12-14 21:42:46 +0330

    home - wrote comments for front-end functions

--- js/lib/mtform.form.js
+++ js/lib/mtform.form.js
@@ -7,40 +7,108 @@
 // FORM COMPONENTS
 // ===================
 
-mtFormInit.prototype.Form = function(args){
-    return this.create("form", args, form_name);
+/**
+ * Creates a form Element. <form>
+ * @param args object @__attributes
+ * @param formKey string the key(name) of the form used across the app. It is great for multi-forms
+ *          app where the elements are destined to specific forms.
+ * @returns {form} @__mtformObject
+ * @constructor
+ */
+mtFormInit.prototype.Form = function(args, formKey){
+    return this.create("form", args, formKey);
 };
 
+/**
+ * Creates a form input (default = text). <input />
+ * @param args object @__attributes
+ * @returns {input} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Input = function(args){
     return this.create("input", args);
 };
 
+/**
+ * Creates a hidden form input. <input type='hidden' />
+ * @param args @__attributes
+ * @returns {hidden} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Hidden = function(args){
     return this.create("hidden", args);
 };
 
+/**
+ * Creates an input password field. <input type='password' />
+ * @param args @__attributes
+ * @returns {password} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Password = function(args){
     return this.create("password", args);
 };
 
+/**
+ * Creates a textarea element. <textarea />
+ * @param args @__attributes
+ * @param innerValue the value which fills content of the element .innerHTML
+ * @returns {textarea} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Textarea = function(args, innerValue){
     return this.create("textarea", args, innerValue);
 };
 
+/**
+ * Creates a submit button. <input type='submit' />
+ * @param args @__attributes
+ * @returns {submit} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Submit = function(args){
     return this.create("submit", args);
 };
 
-mtFormInit.prototype.Button = function(args){
-    return this.create("button", args);
+/**
+ * Creates a button element. <button></button>
+ * @param args @__attributes
+ * @param innerValue the value which fills content of the element .innerHTML
+ * @returns {button} @__mtformObject
+ * @constructor
+ */
+mtFormInit.prototype.Button = function(args, innerValue){
+    return this.create("button", args, innerValue);
 };
 
+/**
+ * Creates a radio-set. It uses a special attributes schema for generating radios.
+ * @param args The attributes for radios are different that the other components.
+ *          The attributes should contain the following elements:
+ *          {
+ *              values :  [object - array]          an array of values based on which the number of radios are
+ *                                                     specified. Since they basically must be unique.
+ *              name :    [string|object - array]   if string, a name for the whole radio set, else, an array,
+         *                                             it will be distributed within the radios. Hence, an array
+         *                                             of two elements for name, for an array of six elements for
+         *                                             values, result in a three sets of radios, each having two
+         *                                             elements.
+ *              default : [string|object - array]   Sets the default value for the radios. If string, checks
+         *                                             against all the components' values. It is logical and standard
+         *                                             for it to be an array of same size as names.
+ *              attrs :   [object - object]         @__attributes
+ *              labels :  [object - array]          If set, then a label for each radio will be created.
+ *          }
+ * @returns {radio} @__mtformObject
+ * @constructor
+ */
 mtFormInit.prototype.Radios = function(args){
     return this.create("radio", args.attrs, args);
 };
 
+
 mtFormInit.prototype.List = function(args){
-    return this.create("radio", args.attrs, args);
+    return this.create("list", args.attrs, args);
 };
 
 /**
