05d40bc6f3d12cec215a5236ed9d41b3ae5e8cbb
Mostafa Talebi <mostafatalebi@rocketmail.com>
2014-12-14 16:44:47 +0330

    office - commit remaining changes

--- js/lib/mtform.dom.js
+++ js/lib/mtform.dom.js
@@ -8,20 +8,29 @@ mtFormInit.prototype.__injectHtml = function()
     // let's see what it will do, currently no decision on it
 }
 
+/**
+ * Inserts HTML contents to be added before the last added component
+ * @param htmlContent the HTML content to be added
+ * @returns {mtFormInit} void
+ * @constructor
+ */
 mtFormInit.prototype.HtmlAfter = function(htmlContent)
 {
     this.isHtml = true;
     this.htmlObject.after = htmlContent;
     this.__addHtmls(this.componentLastInfo);
-    return this;
 }
 
+/**
+ * Inserts HTML contents to be added after the last added component
+ * @param htmlContent the HTML content to be added
+ * @constructor
+ */
 mtFormInit.prototype.HtmlBefore = function(htmlContent)
 {
     this.isHtml = true;
     this.htmlObject.before = htmlContent;
     this.__addHtmls(this.componentLastInfo);
-    return this;
 }
 
 mtFormInit.prototype.__addHtmls = function(component_last_stack_properties)
