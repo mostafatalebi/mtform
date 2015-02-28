c0b1317a9800e0074af2d3f6dea721a59005f454
Mostafa Talebi <most.talebi@gmail.com>
2015-02-28 13:25:40 +0330

    Valid (for validation) plugin created. A test file called valid.html also has been added.

---
+++ js/plugins/Valid/Valid.mtform.extend.js
@@ -0,0 +1,4 @@
+$mtf.extends("Valid", function(){
+    var valid = new MTF_Valid();
+    Vali
+});
---
+++ js/plugins/Valid/Valid.mtform.js
@@ -0,0 +1,12 @@
+var MTF_Valid = function(){
+}
+
+MTF_Valid.prototype.Check = function(){
+
+}
+
+
+/**######## PRIVATE FUNCTIONS #########**/
+MTF_Valid.prototype.__test_rules = function( components ){
+    i
+}
