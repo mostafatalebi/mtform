64354299e0db9382f4dcfd251501401c5fc5b87f
Mostafa Talebi <most.talebi@gmail.com>
2015-03-09 16:48:04 +0330

    Adding a new plugin. This plugin is responsible to get the values of inputs of the form and wrap them in an object. It checks to see if the browser supports FormData or not, if not it fallbacks on another function.

---
+++ js/plugins/FormElement/FormElement.mtform.js
@@ -0,0 +1,106 @@
+/**
+ * This plugin receives all the values of the form's components. It allows exclusion of certain components (by name).
+ * It returns both an object or an JSON-string of the value with the form's name as their keys.
+ */
+
+/**
+ * the id of the form whose components are to be scanned
+ * @param form_id the id of the form or the form element itself
+ * @constructor
+ */
+var MTF_FormElement = function(form_id){
+    if( typeof form_id === 'object' )
+    {
+        this.form = form_id;
+    }
+    else
+    {
+        this.form = document.getElementById(form_id);
+    }
+
+}
+
+
+MTF_FormElement.prototype.getComponents = function(){
+    return this.form.elements;
+}
+
+MTF_FormElement.prototype.getComponentsByAttribute = function(attr_name, element_exclusion_by_name, callback){
+    var form_data;
+    if(window.FormData === 'undefined')
+    {
+        form_data = new FormData(this.form);
+    }
+    else
+    {
+        form_elms = this.getComponents();
+        form_data = {};
+        var obj_keys = [];
+        obj_keys = Object.keys(form_elms);
+        if(!attr_name) attr_name = "value";
+
+        if(element_exclusion_by_name)
+        {
+            for( var i = 0; i < obj_keys.length; i++ )
+            {
+                var element_name = form_elms[obj_keys[i]].name;
+                if( element_exclusion_by_name.indexOf(element_name) )
+                {
+                    if(callback)
+                    {
+                        form_data[element_name] = callback(form_elms[obj_keys[i]]);
+                    }
+                    else
+                    {
+                        if( attr_name == "value" )
+                        {
+                            form_data[element_name] = form_elms[obj_keys[i]].value;
+                        }
+                        else
+                        {
+                            form_data[element_name] = form_elms[obj_keys[i]].getAttribute(attr_name);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            for( var i = 0; i < form_elms.length; i++ )
+            {
+                var element_name = form_elms[i].getAttribute("name");
+
+                if(callback)
+                {
+                    form_data[element_name] = callback(form_elms[i]);
+                }
+                else
+                {
+                    if( attr_name == "value" )
+                    {
+                        form_data[element_name] = form_elms[i].value;
+                    }
+                    else
+                    {
+                        form_data[element_name] = form_elms[i].getAttribute(attr_name);
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    return form_data;
+}
+
+
+MTF_FormElement.prototype.getComponentsValueInJSON = function(element_exclusion_by_name){
+    var form_elements = this.getComponentsByAttribute("value", element_exclusion_by_name);
+    return  JSON.stringify(form_elements);
+}
+
+MTF_FormElement.prototype.jsonToFormElement = function(json_stirng){
+    var form_elements = this.getComponentsByAttribute("value", element_exclusion_by_name);
+    return  JSON.stringify(form_elements);
+}
