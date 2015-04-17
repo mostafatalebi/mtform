7f9e7c0e464a0236aafb42542a8acf84b79d70e3
Mostafa Talebi <most.talebi@gmail.com>
2015-04-17 18:36:41 +0430

    a set of methods to work with elements properties. It has several simple methods and would be probably exended for future use.

---
+++ js/lib/mtform.element.js
@@ -0,0 +1,44 @@
+mtFormInit.prototype.E = function(element)
+{
+    this.Element = element;
+    return {
+        /**
+         * gets the attribute "name" of the element
+         * @returns {string}
+         */
+        getName : function(){
+            return $mtf.Element.getAttribute("name");
+        },
+        /**
+         * Both a setter and a getter function
+         * gets the value of the attribute whose name is passed, if value is passed too, then its sets the
+         * value of that attribute.
+         * @param name
+         * @param value
+         * @returns {*}
+         * @constructor
+         */
+        Attr : function(name, value){
+            if(value)
+            {
+                return $mtf.Element.setAttribute(name, value);
+            }
+            else
+            {
+                return $mtf.Element.getAttribute(name);
+            }
+        },
+        /**
+         *
+         * @returns {string}
+         * @constructor
+         */
+        Value : function(){
+            return $mtf.Element.value;
+        },
+
+        HTML : function(html){
+            $mtf.Element.innerHTML = html;
+        }
+    }
+}
