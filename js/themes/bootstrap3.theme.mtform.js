752aca357e2e1ec0fa7d419584ff75c677a30d69
Mostafa Talebi <most.talebi@gmail.com>
2015-05-15 10:51:19 +0430

    A new directory js/themes has been added for storing themes. These themes must be defined with Theme plugin whose structure has been changed with the new .plugin interface.

---
+++ js/themes/bootstrap3.theme.mtform.js
@@ -0,0 +1,25 @@
+if( $mtf.pluginLoaded( "Theme" ) )
+{
+    var templates =  {
+        form : "<form :attrs >:form</form>",
+        input : "<div class='input-group'><input :attrs /></div>",
+        password : "<div class='input-group'>:component</div>",
+        file : "<div class='input-group'>:component</div>",
+        hidden : "<input type='hidden' :attrs />",
+        textarea : "<div class='input-group'><textarea class='form-control' :attrs >:innerValue</textarea></div>",
+        radio : "<input type='radio' :values  :attrs /><span :label::attrs >:title</span>",
+        checkbox : "<input type='checkbox'   :attrs /><span :label::attrs >:title</span>",
+        submit : "<input type='submit'   :attrs />",
+        button : "<button   :attrs >:value</button>",
+        select : "<select  :attrs >:options</select>",
+        option : "<option  :uniqueValue :attrs />:innerValue</option>",
+        message : "<div :attrs >:message</div>",
+        label : "<label :attrs >:innerValue</label>",
+    };
+
+    $mtf.$lives.Theme.addTheme( "Bootstrap3", templates );
+    $mtf.$lives.Theme.setTheme( "Bootstrap3");
+    $mtf.$lives.Theme.setDefaultAttr("input", { class : "one-such-item"} );
+    $mtf.$lives.Theme.setDefaultAttr("message", { class : "alert alert-danger" /*, style : "display: none;"*/} );
+    $mtf.$lives.Theme.setDefaultAttr("submit", { class : "btn btn-large btn-info"} );
+}
